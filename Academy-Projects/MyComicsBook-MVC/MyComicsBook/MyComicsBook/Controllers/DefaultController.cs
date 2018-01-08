515f4ac286354a597971658bafe0fd39584b310a
Your Name <you@example.com>
2018-01-08 14:22:22 -0800

    Name changing

---
+++ Academy-Projects/MyComicsBook-MVC/MyComicsBook/MyComicsBook/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyComicsBook.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+       {
+            var comicBook = Models.ComicBookManager.GetComicBooks();
+            return View(comicBook);
+        }
+        public ActionResult Detail(int id)
+        {
+            var comicBooks = Models.ComicBookManager.GetComicBooks();
+            var comicBook = comicBooks.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comicBook);
+        }
+    }
+}
---
+++ Academy-Projects/MyComicsBook-MVC/MyComicsBook/MyComicsBook/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyComicsBook.MvcApplication" Language="C#" %>
